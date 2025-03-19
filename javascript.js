988a4a17296c15fe702a18cedac8ebc037b16207
TheVuGl <nguyenthevu13052004@gmail.com>
2025-03-20 03:28:48 +0700

    Them hieu ung thanh keo trang

--- javascript.js
+++ javascript.js
@@ -1,25 +1,38 @@
-$(document).ready(function() {
-    $("#toggle").click(function() {
-        $("nav").slideToggle();
-    });
+$(document).ready(function () {
+  $("#toggle").click(function () {
+    $("nav").slideToggle();
+  });
 });
-document.addEventListener("DOMContentLoaded", function() {
-    const menuLinks = document.querySelectorAll("#main-menu li a");
+document.addEventListener("DOMContentLoaded", function () {
+  const menuLinks = document.querySelectorAll("#main-menu li a");
 
-    menuLinks.forEach((link) => {
-        link.addEventListener("click", function(event) {
-            // Kiểm tra nếu người dùng nhấn vào "JavaScript"
-            if (this.textContent.trim() === "JavaScript") {
-                event.preventDefault(); // Chặn chỉ khi nhấn vào JavaScript
-                const article = document.getElementById("article");
-                article.innerHTML = `
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      // Kiểm tra nếu người dùng nhấn vào "JavaScript"
+      if (this.textContent.trim() === "JavaScript") {
+        event.preventDefault(); // Chặn chỉ khi nhấn vào JavaScript
+        const article = document.getElementById("article");
+        article.innerHTML = `
                     <h2>Bài viết về Object trong JavaScript</h2>
                     <p>
                         Object trong JavaScript là một kiểu dữ liệu quan trọng, giúp lưu trữ các cặp key-value.
                         Một object có thể chứa các thuộc tính (property) và phương thức (method) để xử lý dữ liệu hiệu quả.
                     </p>
                 `;
-            }
-        });
+      }
     });
+  });
+});
+// Thanh keo man hinh
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(window).scrollTop()) {
+      $("#backtop").fadeIn();
+    } else {
+      $("#backtop").fadeOut();
+    }
+  });
+  $("#backtop").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+  });
 });
